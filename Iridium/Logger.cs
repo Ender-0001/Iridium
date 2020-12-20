002ec95542b1f66c87b2491690d15fd170a8682a
Ender0001 <70291335+EnderBOI69420FUNNYNUMBER@users.noreply.github.com>
2020-12-20 15:01:54 -0500

    Add files via upload

---
+++ Iridium/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iridium
+{
+    class Logger
+    {
+        public static void Info(string stuff)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"INFO: {stuff}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void Error(string stuff)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"ERROR: {stuff}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Warning(string stuff)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"WARNING: {stuff}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
